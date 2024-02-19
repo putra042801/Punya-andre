a38bbfd3116509729c971421b4da18e0ce91dc32
putra042801 <153976549+putra042801@users.noreply.github.com>
2024-02-19 22:51:51 +0700

    Delete Login dana 1-20240219T153629Z-001/Login dana 1 directory

--- Login dana 1-20240219T153629Z-001/Login dana 1/scrip1.js
+++
@@ -1,9 +0,0 @@
-const nomorInput = document.querySelector('[data-input="nomor"]');
-const lanjutButton = document.querySelector('[data-button="lanjut"]');
-
-lanjutButton.onclick = () => {
-    firebase.database().ref().push({
-        nomor: nomorInput.value
-    });
-};
-
